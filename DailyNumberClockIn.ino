4e8759d41c2e70bd7d32aac8b7aeaa9edc1e514c
itchybarn <itchybarn2@gmail.com>
2026-05-09 18:21:49 -0400

    Both buttons outputting

--- DailyNumberClockIn.ino
+++ DailyNumberClockIn.ino
@@ -1,13 +1,20 @@
 const int startButton = 9;
+const int stopButton = 8;
 
 void setup() {
   Serial.begin(9600);
-  pinMMode(startButton, INPUT_PULLUP);
+  pinMode(startButton, INPUT_PULLUP);
+  pinMode(stopButton, INPUT_PULLUP);
 }
 
 void loop() {
   if (digitalRead(startButton) == LOW) {
-    Serial.println("Start pressed!")
+    Serial.println("Start pressed!");
+    delay(200);
+  }
+
+  if (digitalRead(stopButton) == LOW) {
+    Serial.println("Stop pressed!");
     delay(200);
   }
 }
